9caf12df48468f726f881a78a8ddd7f348fa636a
promit <promit666@gmail.com>
2020-01-25 16:23:55 +0600

    Add file

---
+++ STRONGN Strong Number (Special Numbers Series #2).py
@@ -0,0 +1,13 @@
+def strong_num(number):
+    string = str(number)
+    total = 0
+    for i in string:
+        num = int(i)
+        tempval = 1
+        for j in range(1, num +1):
+            tempval *= j
+        total += tempval
+    if total == number:
+         return "STRONG!!!!"
+    else:
+        return "Not Strong !!
