99825b7e33bb3201c1d18205508353ade80a1b94
promit <promit666@gmail.com>
2020-01-29 12:26:23 +0600

    Add file

---
+++ Jumping Number (Special Numbers Series #4).py
@@ -0,0 +1,15 @@
+def jumping_number(number):
+    string = str(number)
+    index = 0
+    index2 = 1
+    diff = 1
+    if len(string) > 1:
+        while index2 < len(string):
+            if abs(int(string[index]) - int(string[index2])) != 1:
+                return "Not !!"
+            index += 1
+            index2 += 1
+    return "Jumping !!"
+
+
+print(jumping_number(98789876))
