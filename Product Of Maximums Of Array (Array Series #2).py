6fe501414476c0d391b8484fdf3cbbd616ffbe15
promit <promit666@gmail.com>
2020-11-25 12:57:00 +0600

    Add file

--- Product Of Maximums Of Array (Array Series #2).py
+++ Product Of Maximums Of Array (Array Series #2).py
@@ -2,11 +2,13 @@ def max_product(lst, n_largest_elements):
     output = 1
     lst.sort()
     lst.reverse()
-    nums= []
+    nums = []
     for i in range(0, n_largest_elements):
         nums.append(lst[i])
-    for  i in nums:
+    for i in nums:
         output *= i
     return output
 
-print(max_product([4,3,5], 2))
+
+print(max_product([4, 3, 5], 2))
+
